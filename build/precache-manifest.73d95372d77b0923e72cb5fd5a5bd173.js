5df7b51ffa5d94f7b3fef2e419c1185fb3961894
luisfernandocalvillo <luiscalvillo2184@gmail.com>
2022-07-09 10:47:14 -0600

    subiendo version de produccion

---
+++ build/precache-manifest.73d95372d77b0923e72cb5fd5a5bd173.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7ae90e5f5546be68157",
+    "url": "/static/css/main.78c2eb72.chunk.css"
+  },
+  {
+    "revision": "c7ae90e5f5546be68157",
+    "url": "/static/js/main.c7ae90e5.chunk.js"
+  },
+  {
+    "revision": "3583054c18e22a0ac43d",
+    "url": "/static/js/1.3583054c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e129ca09869e51b6a2960aa0a27ae67",
+    "url": "/static/media/Logo2.8e129ca0.svg"
+  },
+  {
+    "revision": "e6eb60e8744f69c3e23acaff9dc27e3b",
+    "url": "/static/media/download.e6eb60e8.svg"
+  },
+  {
+    "revision": "30638160e11f7f3386e9cfa0c43f06e7",
+    "url": "/static/media/background.30638160.png"
+  },
+  {
+    "revision": "55728cca5edd5d58f1dd2c2970d56db8",
+    "url": "/static/media/presentation.55728cca.png"
+  },
+  {
+    "revision": "b7750e4a471456e536e8daa47849b5a5",
+    "url": "/index.html"
+  }
+];
